942138148a934a99dda68cd3144dc0b0e8b9dea1
chldnrwo <chldnrwo777@naver.com>
2024-05-23 17:59:06 +0900

    front first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!--img alt="Vue logo" src="./assets/logo.png"-->
+  <HelloWorld msg="Vue Test"/>
+  <!--BarChart /-->
+  <BarChart2 />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//import BarChart from './components/BarChart.vue';
+import BarChart2 from './components/BarChart2.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    //BarChart,
+    BarChart2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BarChart2.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="chart-container">
+      <canvas ref="canvas"></canvas>
+    </div>
+  </template>
+  
+  <script>
+  import { Chart, registerables } from 'chart.js';
+  import axios from 'axios';
+  Chart.register(...registerables);
+  
+  export default {
+    name: 'BarChart2',
+    data() {
+      return {
+        stockData: {
+          TSLA: [],
+          NVDA: [],
+          GOOGL: [],
+          AAPL: [],
+          MSFT: [],
+          AMZN: [],
+          META: []
+        }
+      };
+    },
+    mounted() {
+      this.fetchStockData();
+    },
+    methods: {
+      fetchStockData() {
+        axios.get('http://localhost:8080/stocks/AbsM7')
+          .then(response => {
+            console.log('API response:', response.data);
+            this.stockData = response.data;
+            this.renderChart();
+          })
+          .catch(error => {
+            console.error("Error fetching stock data: ", error);
+          });
+      },
+      renderChart() {
+        const ctx = this.$refs.canvas.getContext('2d');
+        
+        // 유효성 검사 추가
+        if (!this.stockData.TSLA || !this.stockData.NVDA || !this.stockData.GOOGL || 
+            !this.stockData.AAPL || !this.stockData.MSFT || !this.stockData.AMZN || !this.stockData.META) {
+          console.error('Stock data is missing');
+          return;
+        }
+  
+        const labels = this.stockData.TSLA.map(data => data.date);
+        
+        const datasets = [
+          {
+            label: 'Tesla Change Percentage',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: this.stockData.TSLA.map(data => data.change)
+          },
+          {
+            label: 'Nvidia Change Percentage',
+            backgroundColor: 'rgba(192, 75, 75, 0.2)',
+            borderColor: 'rgba(192, 75, 75, 1)',
+            borderWidth: 1,
+            data: this.stockData.NVDA.map(data => data.change)
+          },
+          {
+            label: 'Google Change Percentage',
+            backgroundColor: 'rgba(75, 192, 75, 0.2)',
+            borderColor: 'rgba(75, 192, 75, 1)',
+            borderWidth: 1,
+            data: this.stockData.GOOGL.map(data => data.change)
+          },
+          {
+            label: 'Apple Change Percentage',
+            backgroundColor: 'rgba(192, 192, 75, 0.2)',
+            borderColor: 'rgba(192, 192, 75, 1)',
+            borderWidth: 1,
+            data: this.stockData.AAPL.map(data => data.change)
+          },
+          {
+            label: 'Microsoft Change Percentage',
+            backgroundColor: 'rgba(75, 75, 192, 0.2)',
+            borderColor: 'rgba(75, 75, 192, 1)',
+            borderWidth: 1,
+            data: this.stockData.MSFT.map(data => data.change)
+          },
+          {
+            label: 'Amazon Change Percentage',
+            backgroundColor: 'rgba(192, 75, 192, 0.2)',
+            borderColor: 'rgba(192, 75, 192, 1)',
+            borderWidth: 1,
+            data: this.stockData.AMZN.map(data => data.change)
+          },
+          {
+            label: 'Meta Change Percentage',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: this.stockData.META.map(data => data.change)
+          }
+        ];
+  
+        if (ctx) {
+          new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: labels,
+              datasets: datasets
+            },
+            options: {
+              responsive: true,
+              maintainAspectRatio: false,
+              scales: {
+                x: {
+                  ticks: {
+                    maxRotation: 45,
+                    minRotation: 45,
+                    callback: function(value) {
+                      const date = new Date(labels[value]);
+                      //if (date.getDate() === 1) { // 매월 1일만 표시
+                        const options = { year: 'numeric', month: '2-digit' };
+                      return date.toLocaleDateString('en-US', options).replace('/', '/');
+                      //}
+                    }
+                  }
+                },
+                y: {
+                  beginAtZero: false
+                }
+              },
+              plugins: {
+                tooltip: {
+                  callbacks: {
+                    label: function(context) {
+                      let label = context.dataset.label || '';
+                      if (label) {
+                        label += ': ';
+                      }
+                      if (context.parsed.y !== null) {
+                        label += context.parsed.y.toFixed(2) + '%'; // 여기에서 % 기호 추가
+                      }
+                      return label;
+                    }
+                  }
+                }
+              }
+            }
+          });
+        } else {
+          console.error("Canvas context not found.");
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .chart-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 80%;
+    height: 400px;
+    margin: 0 auto;
+  }
+  </style>
+  
